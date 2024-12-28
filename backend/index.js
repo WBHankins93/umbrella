cc52387d3cd5ad33567621e3101a7a0b7122e649
Ben Hankins <benhankins@Bens-MBP.lan>
2024-12-27 18:48:15 -0600

    adding get route to gather weather data

--- backend/index.js
+++ backend/index.js
@@ -15,4 +15,30 @@ app.get('/', (req,res) => {
     res.send('Weather Dashboard Backend');
 });
 
+app.get('/api/weather', async (req,res) => {
+    const { city } = req.query;
+    const apiKey = process.env.WEATHER_API_KEY;
+
+    if (!city) {
+        return res.status(400).json({error: 'City is required'});
+    }
+
+    try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
+            params: {
+                q: city,
+                appid: apiKey,
+                units: 'metric',
+            },
+        });
+        
+        res.json(response.data)
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ error: 'Failed to fetch weather data'})
+    }
+
+})
+
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
